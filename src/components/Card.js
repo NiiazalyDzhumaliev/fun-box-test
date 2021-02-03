0a8f28acb517cb2353ab1a753e460f73446dfb70
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-03 20:37:34 +0600

    Add margin to product description

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,16 @@ import style from '../styles/Card.module.css';
 const Card = () => (
   <div>
     <div className={`${style.card_item}`}>
-      <div>
-        <p className="m-0">Сказочное заморское яство</p>
+      <div className={style.description_container}>
+        <p className={style.product_description}>Сказочное заморское яство</p>
         <h1 className={style.product_name}>Нямушка</h1>
         <h3 className={style.ingredient}>с фуа-гра</h3>
-        <p className="m-0">10 порций</p>
-        <p className="m-0">мышь в подарок</p>
+        <p className="m-0">
+          <span>10</span> порций
+        </p>
+        <p className="m-0">
+          <span>1</span> мышь в подарок
+        </p>
       </div>
       <div className={style.image_container}>
         <img src={cat} alt="cat" className={style.cat_image} />
