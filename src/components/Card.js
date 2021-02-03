516620748bde906425b1cc41dcacb9cc4903d20a
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-03 19:22:37 +0600

    Change font size and color of label

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ const Card = () => (
     <div className={`${style.card_item}`}>
       <img src={cat} alt="cat" className={style.cat_image} />
       <div className={style.weight}>
-        <span>0,5</span>
-        <span>кг</span>
+        <span className={style.weight_amount}>0,5</span>
+        <span className={style.weight_degree}>кг</span>
       </div>
     </div>
   </div>
