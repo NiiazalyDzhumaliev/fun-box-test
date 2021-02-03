7ea5032360a2e13b31cbd127ce140bb9b857f9b5
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-03 16:15:25 +0600

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import style from '../styles/Header.module.css';
 
 const Header = () => (
-  <header className={style.main_header}>
-    <h1>Ты сегодня покормил кота?</h1>
+  <header>
+    <h1 className={style.main_header}>Ты сегодня покормил кота?</h1>
   </header>
 );
 
