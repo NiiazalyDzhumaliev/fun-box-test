8ea7bd2bed515eaeb9597097beb1acfecf7618d8
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-03 19:42:19 +0600

    Add text to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,19 @@ import style from '../styles/Card.module.css';
 const Card = () => (
   <div>
     <div className={`${style.card_item}`}>
-      <img src={cat} alt="cat" className={style.cat_image} />
-      <div className={style.weight}>
-        <span className={style.weight_amount}>0,5</span>
-        <span className={style.weight_degree}>кг</span>
+      <div>
+        <p className="m-0">Сказочное заморское яство</p>
+        <h1 className={style.product_name}>Нямушка</h1>
+        <h3 className={style.ingredient}>с фуа-гра</h3>
+        <p className="m-0">10 порций</p>
+        <p className="m-0">мышь в подарок</p>
+      </div>
+      <div className={style.image_container}>
+        <img src={cat} alt="cat" className={style.cat_image} />
+        <div className={style.weight}>
+          <span className={style.weight_amount}>0,5</span>
+          <span className={style.weight_degree}>кг</span>
+        </div>
       </div>
     </div>
   </div>
