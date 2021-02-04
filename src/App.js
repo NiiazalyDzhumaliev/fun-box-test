3aacb30de5a4dfdaef5192c0d0c05c759da2b8a0
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-04 15:21:06 +0600

    Card change color on click event

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import style from './styles/App.module.css';
 
-const App = () => (
-  <div className={style.App}>
-    <Header />
-    <div className="container">
-      <div className={`${style.card_container} row`}>
-        <Card />
-        <Card />
-        <Card />
+const App = () => {
+  const [borderColor, setBorderColor] = useState('#36b5d1');
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(prevState => !prevState);
+    setBorderColor('#de43b4');
+  };
+  return (
+    <div className={style.App}>
+      <Header />
+      <div className="container">
+        <div className={`${style.card_container} row`}>
+          <Card
+            clicked={clicked}
+            borderColor={borderColor}
+            handleClick={handleClick}
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cat from '../images/cat1.png';
 import style from '../styles/Card.module.css';
 
-const Card = () => (
-  <div className={`${style.card_item}`}>
-    <div className={style.description_container}>
-      <p className={style.product_description}>Сказочное заморское яство</p>
-      <h1 className={style.product_name}>Нямушка</h1>
-      <h3 className={style.ingredient}>с фуа-гра</h3>
-      <p className={style.product_amount}>
-        <span className={style.amount}>10</span>
-        {' '}
-        порций
-      </p>
-      <p className={style.product_amount}>
-        <span className={style.amount}>1</span>
-        {' '}
-        мышь в подарок
-      </p>
-    </div>
-    <div className={style.image_container}>
-      <img src={cat} alt="cat" className={style.cat_image} />
-      <div className={style.weight}>
-        <span className={style.weight_amount}>0,5</span>
-        <span className={style.weight_degree}>кг</span>
+const Card = props => {
+  const { clicked, borderColor, handleClick } = props;
+
+  const borderStyle = clicked
+    ? { border: `3.5px solid ${borderColor}` }
+    : { border: '3.5px solid #36b5d1' };
+
+  const labelStyle = clicked
+    ? { backgroundColor: `${borderColor}` }
+    : { backgroundColor: '#36b5d1' };
+
+  return (
+    <div
+      className={`${style.card_item}`}
+      style={borderStyle}
+      onClick={handleClick}
+      onKeyDown={handleClick}
+      role="button"
+      tabIndex={0}
+    >
+      <div className={style.description_container}>
+        <p className={style.product_description}>Сказочное заморское яство</p>
+        <h1 className={style.product_name}>Нямушка</h1>
+        <h3 className={style.ingredient}>с фуа-гра</h3>
+        <p className={style.product_amount}>
+          <span className={style.amount}>10</span>
+          {' '}
+          порций
+        </p>
+        <p className={style.product_amount}>
+          <span className={style.amount}>1</span>
+          {' '}
+          мышь в подарок
+        </p>
+      </div>
+      <div className={style.image_container}>
+        <img src={cat} alt="cat" className={style.cat_image} />
+        <div className={style.weight} style={labelStyle}>
+          <span className={style.weight_amount}>0,5</span>
+          <span className={style.weight_degree}>кг</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Card.propTypes = {
+  clicked: PropTypes.bool.isRequired,
+  borderColor: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Card;
