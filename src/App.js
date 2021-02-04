81bbd66edc37777b5a57398508e6fcf715ebb445
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-02-04 18:27:43 +0600

    Change color on click and hover events

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import style from './styles/App.module.css';
 
 const App = () => {
   const [clicked, setClicked] = useState(false);
+  const [mouseEntered, setMouseEntered] = useState(false);
+  const [mouseLeft, setMouseLeft] = useState(true);
+  const [firstSelect, setFirstSelect] = useState(false);
+
+  const handleMouseEnter = () => {
+    setMouseEntered(prevState => !prevState);
+    setMouseLeft(prevState => !prevState);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseLeft(prevState => !prevState);
+    setMouseEntered(prevState => !prevState);
+    setFirstSelect(false);
+  };
 
   const handleClick = () => {
     setClicked(prevState => !prevState);
+    setFirstSelect(prevState => !prevState);
   };
   return (
     <div className={style.App}>
       <Header />
       <div className="container">
         <div className={`${style.card_container} row`}>
-          <Card clicked={clicked} handleClick={handleClick} />
+          <Card
+            clicked={clicked}
+            handleClick={handleClick}
+            handleMouseEnter={handleMouseEnter}
+            handleMouseLeave={handleMouseLeave}
+            mouseEntered={mouseEntered}
+            mouseLeft={mouseLeft}
+            firstSelect={firstSelect}
+          />
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,27 @@ import cat from '../images/cat1.png';
 import style from '../styles/Card.module.css';
 
 const Card = props => {
-  const { clicked, handleClick } = props;
+  const {
+    clicked,
+    handleMouseEnter,
+    handleClick,
+    mouseEntered,
+    handleMouseLeave,
+    mouseLeft,
+    firstSelect,
+  } = props;
 
   const borderStyle = clicked
     ? { border: '3.5px solid #de43b4' }
     : { border: '3.5px solid #36b5d1' };
+
+  const borderHoverStyle = {
+    border: '3.5px solid #f582ae',
+  };
+
+  const labelHoverStyle = {
+    backgroundColor: '#f582ae',
+  };
 
   const labelStyle = clicked
     ? { backgroundColor: '#de43b4' }
@@ -17,8 +33,14 @@ const Card = props => {
   return (
     <div
       className={`${style.card_item}`}
-      style={borderStyle}
+      style={
+        clicked && mouseEntered && !mouseLeft && !firstSelect
+          ? borderHoverStyle
+          : borderStyle
+      }
       onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       onKeyDown={handleClick}
       role="button"
       tabIndex={0}
@@ -40,7 +62,14 @@ const Card = props => {
       </div>
       <div className={style.image_container}>
         <img src={cat} alt="cat" className={style.cat_image} />
-        <div className={style.weight} style={labelStyle}>
+        <div
+          className={style.weight}
+          style={
+            clicked && mouseEntered && !mouseLeft && !firstSelect
+              ? labelHoverStyle
+              : labelStyle
+          }
+        >
           <span className={style.weight_amount}>0,5</span>
           <span className={style.weight_degree}>кг</span>
         </div>
@@ -51,7 +80,12 @@ const Card = props => {
 
 Card.propTypes = {
   clicked: PropTypes.bool.isRequired,
+  handleMouseEnter: PropTypes.func.isRequired,
+  handleMouseLeave: PropTypes.func.isRequired,
   handleClick: PropTypes.func.isRequired,
+  mouseEntered: PropTypes.bool.isRequired,
+  mouseLeft: PropTypes.bool.isRequired,
+  firstSelect: PropTypes.bool.isRequired,
 };
 
 export default Card;
